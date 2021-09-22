c5644710c133eba8b4c890877c2f1e9307b4ec52
Shyam Prag <pragshyam11@gmail.com>
2021-09-22 22:54:40 +0200

    add styling to profile page

--- src/profile/ProfileHeader.js
+++ src/profile/ProfileHeader.js
@@ -1,7 +1,12 @@
 export default function ProfileHeader(props) {
-    
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
     return (
-        <div className="profile-banner">
+        <div className="profile-header">
             <div className="profile-content">
                 <div className="row">
 
@@ -9,7 +14,7 @@ export default function ProfileHeader(props) {
                         <img alt="logo" className="project-logo" src="https://firebasestorage.googleapis.com/v0/b/ideas-4c887.appspot.com/o/icon-vervet.png?alt=media&token=47ef7b2c-ace6-490a-bdbf-f74558bb6f40" />
                     </div>
                     <div className="col-md-10 profile-header-text">
-                        <p className="profile-header-name">{props.ideaName}</p>
+                        <p className="profile-header-name"> {props.ideaName} </p>
                         <p className="profile-header-caption">{props.subtitle}</p>
                         <p className="profile-header-date-created">Date Created: 19 September 2021</p>
                     </div>
--- src/profile/ProfileNav.js
+++ src/profile/ProfileNav.js
@@ -7,14 +7,17 @@ export default function ProfileHeader(props) {
                     <div className="col-md-2"></div>
                     <div className="col-md-8">
                         <div className="row profile-nav-bar">
-                            <div className="col-md-4 selected">
-                                <p>About</p>
+                            <div className="col-md-3 profile-nav-bar-item">
+                                <p>ABOUT</p>
                             </div>
-                            <div className="col-md-4">
-                                <p>Timeline</p>
+                            <div className="col-md-3 profile-nav-bar-item">
+                                <p>RESOURCES</p>
                             </div>
-                            <div className="col-md-4">
-                                <p>Collaborators</p>
+                            <div className="col-md-3 profile-nav-bar-item">
+                                <p>TIMELINE</p>
+                            </div>
+                            <div className="col-md-3 profile-nav-bar-item">
+                                <p>COLLABORATORS</p>
                             </div>
                         </div>
                     </div>
